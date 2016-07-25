dfe2aa2ad27641e1f9d548a571e939800ecc3e52
ZAstafyeva <z.a.astafyeva@gmail.com>
2016-07-25 20:37:33 +1000

    Add new solution to old quiz

--- basic-algorithms/reverse-a-string.js
+++ basic-algorithms/reverse-a-string.js
@@ -1,7 +1,17 @@
 //Reverse the provided string.
 
+//Using built-in methods
 function reverseString(str) {
   return str.split('').reverse().join('');
 }
 
 reverseString("hello");
+
+//Using for-loop
+function reverseString(str) {
+  var reversedStr = '';
+    for (var i = str.length - 1; i >= 0; i--) {
+      reversedStr += str.charAt(i);
+    }
+  return reversedStr;
+}
