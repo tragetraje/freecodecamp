adb104ffe1ba651543c9e8094bf671975f3826fe
ZAstafyeva <z.a.astafyeva@gmail.com>
2016-07-30 09:10:04 +1000

    Solve another algorithm

---
+++ basic-algorithms/slasher-flick.js
@@ -0,0 +1,8 @@
+//Return the remaining elements of an array after chopping off n elements from the head
+
+function slasher(arr, howMany) {
+  // it doesn't always pay to be first
+  return arr.splice(howMany);
+}
+
+slasher([1, 2, 3], 2);
