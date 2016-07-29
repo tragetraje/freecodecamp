34e46b94d3128cc35bbb6bb835db293f583f4869
ZAstafyeva <z.a.astafyeva@gmail.com>
2016-07-29 16:26:04 +1000

    Add cleaner solution for Chunky Monkey

--- basic-algorithms/chunky-monkey.js
+++ basic-algorithms/chunky-monkey.js
@@ -6,10 +6,20 @@ function chunkArrayInGroups(arr, size) {
   chunkArr.push(arr.slice(0, size));
 
   //After pushing the first chunk to chunkArr let arr equal
-  //remaining part of the arr 
+  //remaining part of the arr
   arr = arr.slice(size);
   }
   return chunkArr;
 }
+
+//Another solution
+function chunkArrayInGroups(arr, size) {
+  var chunkArr = [];
+  	for (var i = 0; i < arr.length; i+=size) {
+  chunkArr.push(arr.slice(i, i + size));
+  }
+  return chunkArr;
+}
+
 
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
