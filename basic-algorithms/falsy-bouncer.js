2f03b5472b50b7ba40df2c71fafe3223c8f54919
ZAstafyeva <z.a.astafyeva@gmail.com>
2016-07-30 20:39:57 +1000

    More solutions

--- basic-algorithms/falsy-bouncer.js
+++ basic-algorithms/falsy-bouncer.js
@@ -2,12 +2,33 @@
 //Falsy values in JavaScript are false, null, 0, "", undefined, and NaN
 
 function bouncer(arr) {
-  // Don't show a false ID to this bouncer.
+    // Don't show a false ID to this bouncer.
 
-  var filtered = arr.filter(function(val) {
-  	return val;
-  });
-  return filtered;
+    var filtered = arr.filter(function(val) {
+        //Statement is equal to Boolean(val) which is false, filteres out
+        //falsy values
+        return val;
+    });
+    return filtered;
 }
 
 bouncer([7, "ate", "", false, 9]);
+
+//Another solution
+
+function bouncer(arr) {
+    // Don't show a false ID to this bouncer.
+    var filtered = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i]) {
+            filtered.push(arr[i]);
+        }
+    }
+    return filtered;
+}
+
+//And yet another
+
+function bouncer(arr) {
+    return arr.filter(Boolean);
+}
