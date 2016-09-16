a6ec323f2e818b5b6dcd834d783c602ae1d541b3
ZAstafyeva <z.a.astafyeva@gmail.com>
2016-09-16 16:40:50 +1000

    Use ipinfo to get location info

--- show-local-weather/script.js
+++ show-local-weather/script.js
@@ -1,24 +1,25 @@
-function getLocationData() {
-    var output = document.getElementById("output");
-
-    if (!navigator.geolocation) {
-        output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-        return;
-    }
-
-    function success(position) {
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
-
-        output.innerHTML = '<p>Latitude is ' + latitude + '<br>Longitude is ' + longitude + '</p>';
-    }
-
-    function error() {
-        output.innerHTML = 'Unable to retrieve your location';
-    }
-
-    output.innerHTML = '<p>Locating...</p>';
-
-    navigator.geolocation.getCurrentPosition(success, error);
-
+function loadData() {
+  var urlLocApi = 'http://ipinfo.io';
+  $.getJSON(urlLocApi, function(location){
+  var coordinates = location.loc.split(',');
+  var city = location.city;
+  var lat = coordinates[0];
+  var lon = coordinates[1];
+  //console.log(city);
+  var urlWeatherApi = 'http://api.openweathermap.org/data/2.5/weather';
+  urlWeatherApi += '?' + $.param({
+   'lat': lat,
+   'lon': lon,
+    'units': 'metric',
+    'appid': 'd988397f6b657b836b5abc918b43b7f0'
+});
+    $.getJSON(urlWeatherApi, function(forecast){
+      var condition = forecast.weather[0].main;
+      var temperature = forecast.main.temp;
+      //console.log('Weather condition: ' + condition + " It's: " + temperature);
+    });
+});
+    return false;
 }
+
+$('#container').submit(loadData);
