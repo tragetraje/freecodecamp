8c5b16924744b0303084f85e3fb71e03cd314e65
ZAstafyeva <z.a.astafyeva@gmail.com>
2016-09-19 12:30:39 +1000

    Wrap in document.ready

--- show-local-weather/script.js
+++ show-local-weather/script.js
@@ -1,25 +1,76 @@
-function loadData() {
-  var urlLocApi = 'http://ipinfo.io';
-  $.getJSON(urlLocApi, function(location){
-  var coordinates = location.loc.split(',');
-  var city = location.city;
-  var lat = coordinates[0];
-  var lon = coordinates[1];
-  //console.log(city);
-  var urlWeatherApi = 'http://api.openweathermap.org/data/2.5/weather';
-  urlWeatherApi += '?' + $.param({
-   'lat': lat,
-   'lon': lon,
-    'units': 'metric',
-    'appid': 'd988397f6b657b836b5abc918b43b7f0'
-});
-    $.getJSON(urlWeatherApi, function(forecast){
-      var condition = forecast.weather[0].main;
-      var temperature = forecast.main.temp;
-      //console.log('Weather condition: ' + condition + " It's: " + temperature);
+$( document ).ready(function() {
+    var urlLocApi = 'http://ipinfo.io';
+    $.getJSON(urlLocApi, function(location) {
+        var coordinates = location.loc.split(',');
+        var lat = coordinates[0];
+        var lon = coordinates[1];
+        var city = location.city;
+        var html = '';
+        var urlWeatherApi = 'http://api.openweathermap.org/data/2.5/weather';
+        urlWeatherApi += '?' + $.param({
+            'lat': lat,
+            'lon': lon,
+            'units': 'metric',
+            'appid': 'd988397f6b657b836b5abc918b43b7f0'
+        });
+        $.getJSON(urlWeatherApi, function(forecast) {
+            var condition = forecast.weather[0].main;
+            var celsius = forecast.main.temp;
+            var weather = '';
+            var bkgImg = '';
+            //console.log(forecast.weather[0].main);
+            //console.log(forecast.main.temp);
+            switch (condition) {
+                case "Clouds":
+                    weather = "Cloudy.";
+                    bkgImg = 'bg-clouds';
+                    break;
+                case "Clear":
+                    weather = "The sky is clear.";
+                    bkgImg = 'bg-clear';
+                    break;
+                case "Rain":
+                    weather = "It's raining.";
+                    bkgImg = 'bg-rain';
+                    break;
+                case "Snow":
+                    weather = "It's snowing.";
+                    bkgImg = 'bg-snow';
+                    break;
+                case "Extreme":
+                    weather = "Caution! Extreme weather conditions.";
+                    bkgImg = 'bg-extreme';
+                    break;
+                default:
+                    weather = "Sorry, not sure what " + condition.toLowerCase() + " means.";
+                    bkgImg = 'bg-default';
+            }
+
+            function convertTo(celsius) {
+                var fahrenheit = '';
+                return fahrenheit = Math.round((celsius * 9 / 5) + 32);
+            }
+            $('.information').text('Hello ' + city + '!');
+            $('.btn-weather').click(function() {
+                $('body').removeClass('bg-default').addClass(bkgImg);
+                $('.btn-weather').hide();
+                $('.btn-fahrenheit').show();
+                $('.information').text("It's " + Math.round(celsius) + "°C in " + city + ". ")
+                    .append(weather);
+            });
+            $('.btn-celsius').click(function() {
+                $('.btn-celsius').hide();
+                $('.btn-weather').hide();
+                $('.btn-fahrenheit').show();
+                $('.information').text("It's " + Math.round(celsius) + "°C in " + city + ". ")
+                    .append(weather);
+            });
+            $('.btn-fahrenheit').click(function() {
+                $('.btn-fahrenheit').hide();
+                $('.btn-celsius').show();
+                $('.information').text("It's " + convertTo(celsius) + "°F in " + city + ". ")
+                    .append(weather);
+            });
+        });
     });
 });
-    return false;
-}
-
-$('#container').submit(loadData);
